8d8ca47b8f7a9f1f5b4b867e3b4414dd6e782db7
Kedlawv <waldek.bus@gmail.com>
2020-06-14 07:34:47 +0200

    Refactor ProjectileController to raise a OutOfBounds event instead of direct method call to PlayerProjectile.EnableProjectile() | PlayerController subscribes EnableProjectile() to the OutOfBounds event | PlayerController instantiates the projectile so it has a referance to it , Projectile no longer needs a reference to the PlayerController. The two classes are loosely coupled in result.

--- ObserverDemo/Assets/Scripts/ProjectileController.cs
+++ ObserverDemo/Assets/Scripts/ProjectileController.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 
+public delegate void OutOfBoundsHandler();
 public class ProjectileController : MonoBehaviour
 {
     #region Field Declarations
@@ -9,6 +10,8 @@ public class ProjectileController : MonoBehaviour
     public bool isPlayers;
 
     #endregion
+
+    public event OutOfBoundsHandler ProjectileOutOfBounds;
 
     #region Movement
 
@@ -26,8 +29,10 @@ public class ProjectileController : MonoBehaviour
         {
             if(isPlayers == true)
             {
-                PlayerController playerShip = FindObjectOfType<PlayerController>();
-                playerShip.EnableProjectile();
+                if(ProjectileOutOfBounds != null)
+                {
+                    ProjectileOutOfBounds();
+                }
             }
             Destroy(gameObject);
         }
